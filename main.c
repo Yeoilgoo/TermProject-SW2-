97fb6c19c14a6f7420e8e9918064783cd0ac86d6
Yeoilgoo <32262904+Yeoilgoo@users.noreply.github.com>
2017-12-07 00:37:20 +0900

    final

--- main.c
+++ main.c
@@ -1,47 +1,24 @@
 #include "header.h"
 
-void findfile(const char *dir, int tap);
-
 int main() {
-	unsigned num;
+	int count = 0;
 
-	findfile("E:\\users\\ilgoo\\desktop\\FileList", 0);
-	getch();
+	/*검색 키워드 입력*/
 
-	return 0;
-}
+	printf("검색할 키워드를 입력하세요: ");
+	gets(word); 
 
-void findfile(const char *dir, int tap) {
-	struct _finddata_t fd;
-	long handle;
-	int result = 0;
-	char buf[_MAX_PATH + 3], buf2[40];
-	int i;
+	findfile();
 
-	sprintf(buf, "%s\\*.*", dir);
-	handle = _findfirst(buf, &fd);
+	/*결과 출력*/
+	while (head != NULL) {
+		printf("파일명 : %s, 키워드 개수 : %d \n", head->filename, head->num);
+		count++;
 
-	if (handle == -1)
-		return;
-
-	while (result != -1) {
-		if ((fd.attrib & _A_SYSTEM) == 0 && (strcmp(fd.name, ".") != 0) && (strcmp(fd.name, "..")) != 0) {
-			for (i = 0; i <= tap; i++)
-				buf2[i] = ' ';
-
-			buf2[i] = NULL;
-
-			sprintf(buf, "%s파일명 : %s, 크기 : %d", buf2, fd.name, fd.size);
-			puts(buf);
-
-			if ((fd.attrib & _A_SUBDIR) != 0) {
-				sprintf(buf, "%s\\%s", dir, fd.name);
-				findfile(buf, tap + 1);
-			}
-		}
-
-		result = _findnext(handle, &fd);
+		head = head->next;
 	}
 
-	_findclose(handle);
+	printf("\n키워드 %s를 포함하고 있는 %d개의 파일을 찾았습니다!\n", word, count);
+
+	return 0;
 }
